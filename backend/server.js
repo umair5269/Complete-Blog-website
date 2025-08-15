1ac92977b251572bddc17bb8bd488fbd4d91b646
umair5269 <m.umair5269@yahoo.com>
2025-08-15 15:54:14 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,12 @@ const authRoutes = require('./routes/auth');
 const postRoutes=require ('./routes/postRoutes')
 const adminRoutes = require('./routes/admin');
 const managerRoutes = require('./routes/manager');
+const helmet =require('helmet');
+const sanitizeHtml= require('sanitize-html');
 
 const app = express();
+
+app.use(helmet());
 
 // middleware
 app.use(cors());
@@ -21,6 +25,17 @@ app.use('/api/manager', managerRoutes);
 // basic test route
 app.get('/', (req, res) => {
   res.send('API is running...');
+});
+
+// Optional: sanitize HTML input on specific routes
+app.post('/comments', (req, res) => {
+  const cleanComment = sanitizeHtml(req.body.comment, {
+    allowedTags: ['b', 'i', 'em', 'strong', 'a'],
+    allowedAttributes: { 'a': ['href'] }
+  });
+
+  // save cleanComment to DB...
+  res.json({ message: 'Comment saved!', comment: cleanComment });
 });
 
 // connect to MongoDB
